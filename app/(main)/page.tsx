9835cdb9805a92008ba48a4eaec1931317adc22a
Khalid Hamdy <khalid910109@gmail.com>
2026-03-30 15:10:02 +0200

    fix: changed the structure of the components in folders

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,8 +1,8 @@
-import Certifications from "@/components/pages/home/certifications";
-import AboutSection from "@/components/pages/home/aboutSection";
-import Education from "@/components/pages/home/eduction";
-import Projects from "@/components/pages/home/projects";
-import Skills from "@/components/pages/home/skills";
+import Certifications from "@/components/pages/main/certifications";
+import AboutSection from "@/components/pages/main/aboutSection";
+import Education from "@/components/pages/main/eduction";
+import Projects from "@/components/pages/main/projects";
+import Skills from "@/components/pages/main/skills";
 
 export default function Home() {
   return (
